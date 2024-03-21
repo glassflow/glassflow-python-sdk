4a2e42abb08ff7ff1d7f5f418ca67f5a95866601
Ashish Bagri <ashish@glassflow.dev>
2024-03-21 17:58:42 +0100

    actions version upgrades

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except FileNotFoundError:
 
 setuptools.setup(
     name="glassflow",
-    version="0.1.4",
+    version="0.1.5",
     author="glassflow",
     description="GlassFlow Python Client SDK",
     long_description=long_description,
