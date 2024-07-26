0bf19eed8b029dc7d68ad7fbe0824c2a086894c2
Ashish Bagri <ashish@glassflow.dev>
2024-07-26 13:16:04 +0200

    bump version for DEV-125

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except FileNotFoundError:
 
 setuptools.setup(
     name="glassflow",
-    version="1.0.7",
+    version="1.0.8",
     author="glassflow",
     description="GlassFlow Python Client SDK",
     url="https://learn.glassflow.dev/docs",
