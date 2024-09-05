f15d4a6425b8115ae7024d7f9b8b837412e0cb79
Pablo Pardo Garcia <pablo@glassflow.dev>
2024-09-05 16:48:39 +0200

    bump version for DEV-177

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except FileNotFoundError:
 
 setuptools.setup(
     name="glassflow",
-    version="1.0.9",
+    version="1.0.10",
     author="glassflow",
     description="GlassFlow Python Client SDK",
     url="https://learn.glassflow.dev/docs",
