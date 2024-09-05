1b5bd149eda365eae97930117c9b3bc817b29366
Pablo Pardo Garcia <pablo@glassflow.dev>
2024-09-05 15:18:16 +0200

    Add method to check access token status

--- src/glassflow/models/operations/__init__.py
+++ src/glassflow/models/operations/__init__.py
@@ -1,10 +1,11 @@
 from .publishevent import *
 from .consumeevent import *
 from .consumefailed import *
+from .status_access_token import *
 
 __all__ = [
     "PublishEventRequest", "PublishEventRequestBody", "PublishEventResponse",
     "PublishEventResponseBody", "ConsumeEventRequest", "ConsumeEventResponse",
     "ConsumeEventResponseBody", "ConsumeFailedRequest",
-    "ConsumeFailedResponse", "ConsumeFailedResponseBody"
+    "ConsumeFailedResponse", "ConsumeFailedResponseBody", "StatusAccessTokenRequest"
 ]
