ca2024a5e78af38f3fd848cbbbea596425c39d41
Pablo Pardo Garcia <pablo@glassflow.dev>
2024-09-18 17:15:54 +0200

    add tests for PipelineDataSink and PipelineDataSource and use staging api url on integration tests

--- tests/glassflow/conftest.py
+++ tests/glassflow/conftest.py
@@ -3,12 +3,50 @@ import uuid
 
 import pytest
 
-from glassflow.client import GlassFlowClient
+from glassflow import GlassFlowClient, PipelineDataSink, PipelineDataSource
+from glassflow.client import GlassFlowConfig
 
 
 @pytest.fixture
-def client():
-    return GlassFlowClient()
+def staging_config():
+    config = GlassFlowConfig()
+    config.server_url = "https://staging.api.glassflow.dev/v1"
+    return config
+
+
+@pytest.fixture
+def client(staging_config):
+    c = GlassFlowClient()
+    c.glassflow_config = staging_config
+    return c
+
+
+@pytest.fixture
+def source(pipeline_credentials, staging_config):
+    source = PipelineDataSource(**pipeline_credentials)
+    source.glassflow_config = staging_config
+    return source
+
+
+@pytest.fixture
+def source_with_invalid_access_token(pipeline_credentials_invalid_token, staging_config):
+    source = PipelineDataSource(**pipeline_credentials_invalid_token)
+    source.glassflow_config = staging_config
+    return source
+
+
+@pytest.fixture
+def source_with_non_existing_id(pipeline_credentials_invalid_id, staging_config):
+    source = PipelineDataSource(**pipeline_credentials_invalid_id)
+    source.glassflow_config = staging_config
+    return source
+
+
+@pytest.fixture
+def sink(pipeline_credentials, staging_config):
+    sink = PipelineDataSink(**pipeline_credentials)
+    sink.glassflow_config = staging_config
+    return sink
 
 
 @pytest.fixture
--- tests/glassflow/integration_tests/pipeline_test.py
+++ tests/glassflow/integration_tests/pipeline_test.py
@@ -1,13 +1,34 @@
 import pytest
 
-from glassflow.models.errors import ClientError
+from glassflow import errors
 
 
-def test_pipeline_is_access_token_valid_ok(client, pipeline_credentials):
-    pipeline = client.pipeline_client(**pipeline_credentials)
+def test_pipeline_data_source_validate_credentials_ok(source):
+    try:
+        source.validate_credentials()
+    except Exception as e:
+        pytest.fail(e)
 
-    is_valid = pipeline.is_access_token_valid()
-    assert is_valid
+
+def test_pipeline_data_source_validate_credentials_invalid_access_token(source_with_invalid_access_token):
+    with pytest.raises(errors.PipelineAccessTokenInvalidError):
+        source_with_invalid_access_token.validate_credentials()
+
+
+def test_pipeline_data_source_validate_credentials_id_not_found(source_with_non_existing_id):
+    with pytest.raises(errors.PipelineNotFoundError):
+        source_with_non_existing_id.validate_credentials()
+
+
+def test_pipeline_publish_and_consume(source, sink):
+    publish_response = source.publish({"test-key": "test-value"})
+    assert publish_response.status_code == 200
+    while True:
+        consume_response = sink.consume()
+        assert consume_response.status_code in (200, 204)
+        if consume_response.status_code == 200:
+            assert consume_response.json() == {"test-key": "test-value"}
+            break
 
 
 def test_pipeline_is_access_token_valid_not_ok(
@@ -24,7 +45,7 @@ def test_pipeline_is_access_token_valid_with_invalid_credentials(
 ):
     pipeline = client.pipeline_client(**pipeline_credentials_invalid_id)
 
-    with pytest.raises(ClientError) as exc_info:
+    with pytest.raises(errors.ClientError) as exc_info:
         pipeline.is_access_token_valid()
     exc = exc_info.value
     assert exc.status_code == 404
@@ -54,7 +75,7 @@ def test_pipeline_is_valid_ok(
     assert pipeline.is_valid() is True
 
 
-def test_pipeline_publish_and_consume(client, pipeline_credentials):
+def test_pipeline_publish_and_consume_deprecated(client, pipeline_credentials):
     pipeline = client.pipeline_client(**pipeline_credentials)
     publish_response = pipeline.publish({"test-key": "test-value"})
     assert publish_response.status_code == 200
