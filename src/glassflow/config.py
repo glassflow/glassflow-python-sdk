bb58e1fc3b0f4ea0374d6c7bf03961a0e70f3638
Pablo Pardo Garcia <pablo@glassflow.dev>
2024-09-18 13:12:30 +0200

    Remove client from GlassFlowConfig

--- src/glassflow/config.py
+++ src/glassflow/config.py
@@ -9,14 +9,12 @@ class GlassFlowConfig:
     """Configuration object for GlassFlowClient
 
     Attributes:
-        client: requests.Session object to interact with the GlassFlow API
         server_url: The base URL of the GlassFlow API
         sdk_version: The version of the GlassFlow Python SDK
         user_agent: The user agent to be used in the requests
 
     """
 
-    client: requests.Session
     server_url: str = "https://api.glassflow.dev/v1"
     sdk_version: str = version("glassflow")
     user_agent: str = "glassflow-python-sdk/{}".format(sdk_version)
