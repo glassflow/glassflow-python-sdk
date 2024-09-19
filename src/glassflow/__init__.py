0c04ae45ce07d182397b5cf6f4eb207d6aa2be1e
Pablo Pardo Garcia <pablo@glassflow.dev>
2024-09-19 18:45:51 +0200

    move request logic to APIClient

--- src/glassflow/__init__.py
+++ src/glassflow/__init__.py
@@ -1,4 +1,4 @@
 from .client import GlassFlowClient as GlassFlowClient
 from .config import GlassFlowConfig as GlassFlowConfig
 from .models import errors as errors
-from .pipelines import PipelineDataSource as PipelineDataSource, PipelineDataSink as PipelineDataSink
+from .pipeline_data import PipelineDataSource as PipelineDataSource, PipelineDataSink as PipelineDataSink
