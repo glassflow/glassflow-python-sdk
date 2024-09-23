06f7e40b5986ba3f368c1fc7e0c6725340505d23
Pablo Pardo Garcia <pablo@glassflow.dev>
2024-09-23 17:37:35 +0200

    add unittest for pipeline creation with none existing transformation file

---
+++ tests/glassflow/unit_tests/pipeline_test.py
@@ -0,0 +1,11 @@
+import pytest
+
+from glassflow.pipeline import Pipeline
+
+
+def test_pipeline_transformation_file_not_found():
+    with pytest.raises(FileNotFoundError):
+        Pipeline(
+            transformation_file="fake_file.py",
+            personal_access_token="test-token"
+        )
