f5fd098734faa33ff884bf874e243bae64a6ddf1
Pablo Pardo Garcia <pablo@glassflow.dev>
2024-09-25 17:01:26 +0200

    expose Pipeline constructor in top level

--- src/glassflow/__init__.py
+++ src/glassflow/__init__.py
@@ -1,5 +1,6 @@
 from .client import GlassFlowClient as GlassFlowClient
 from .config import GlassFlowConfig as GlassFlowConfig
 from .models import errors as errors
+from .pipeline import Pipeline as Pipeline
 from .pipeline_data import PipelineDataSink as PipelineDataSink
 from .pipeline_data import PipelineDataSource as PipelineDataSource
