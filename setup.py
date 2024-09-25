4403bc9c75fcfbff0e2a661c7e46b8195c966998
Pablo Pardo Garcia <pablo@glassflow.dev>
2024-09-25 17:03:33 +0200

    change version to 2.0.0rc

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except FileNotFoundError:
 
 setuptools.setup(
     name="glassflow",
-    version="2.0.0",
+    version="2.0.0rc",
     author="glassflow",
     description="GlassFlow Python Client SDK",
     url="https://learn.glassflow.dev/docs",
