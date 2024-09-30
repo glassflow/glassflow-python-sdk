32582f236361e8230647349db4f3074580c2b0c4
Pablo Pardo Garcia <pablo@glassflow.dev>
2024-09-30 10:12:08 +0900

    create pipeline before testing get_pipeline

--- tests/glassflow/integration_tests/client_test.py
+++ tests/glassflow/integration_tests/client_test.py
@@ -1,8 +1,8 @@
-def test_get_pipeline_ok(client):
-    pipeline_id = "bdbbd7c4-6f13-4241-b0b6-da142893988d"
+def test_get_pipeline_ok(client, creating_pipeline):
+    p = client.get_pipeline(pipeline_id=creating_pipeline.id)
 
-    p = client.get_pipeline(pipeline_id="bdbbd7c4-6f13-4241-b0b6-da142893988d")
-    assert p.id == pipeline_id
+    assert p.id == creating_pipeline.id
+    assert p.name == creating_pipeline.name
 
 
 def test_list_pipelines_ok(client, creating_pipeline):
