5a16858b0edb6b992c380c89f78e4c71a9232003
Pablo Pardo Garcia <pablo@glassflow.dev>
2024-10-08 11:36:08 +0900

    bump rc version to 2.0.2rc7

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except FileNotFoundError:
 
 setuptools.setup(
     name="glassflow",
-    version="2.0.2rc6",
+    version="2.0.2rc7",
     author="glassflow",
     description="GlassFlow Python Client SDK",
     url="https://learn.glassflow.dev/docs",
