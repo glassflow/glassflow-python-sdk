fcdb0d1a263a040eb08584c6e64974a8ad6af4b2
Pablo Pardo Garcia <pablo@glassflow.dev>
2025-02-10 17:27:36 +0100

    add deleted __init__

---
+++ src/glassflow/models/operations/__init__.py
@@ -0,0 +1,11 @@
+from glassflow.models.operations.pipeline import (
+    CreatePipelineResponse,
+    FetchPipelineResponse,
+    UpdatePipelineRequest,
+)
+
+__all__ = [
+    "CreatePipelineResponse",
+    "FetchPipelineResponse",
+    "UpdatePipelineRequest",
+]
