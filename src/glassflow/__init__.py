121b7f3df47dc79e7e8082c5dfe448a094478e58
Pablo Pardo Garcia <pablo@glassflow.dev>
2025-02-11 16:50:41 +0100

    add secret top level object

--- src/glassflow/__init__.py
+++ src/glassflow/__init__.py
@@ -7,3 +7,4 @@ from .pipeline import Pipeline as Pipeline
 from .pipeline_data import PipelineDataSink as PipelineDataSink
 from .pipeline_data import PipelineDataSource as PipelineDataSource
 from .space import Space as Space
+from .secret import Secret as Secret
---
+++ src/glassflow/secret.py
@@ -0,0 +1,105 @@
+from __future__ import annotations
+
+from .api_client import APIClient
+from .models import api, errors
+
+
+class Secret(APIClient):
+    def __init__(
+        self,
+        personal_access_token: str,
+        key: str | None = None,
+        value: str | None = None,
+        organization_id: str | None = None,
+    ):
+        """
+        Creates a new Glassflow Secret object
+
+        Args:
+            personal_access_token: The personal access token to authenticate
+                against GlassFlow
+            key: Name of the secret
+            value: Value of the secret to store
+        """
+        super().__init__()
+        self.personal_access_token = personal_access_token
+        self.organization_id = organization_id
+        self.key = key
+        self.value = value
+        self.headers = {"Personal-Access-Token": self.personal_access_token}
+        self.query_params = {"organization_id": self.organization_id}
+
+    def create(self) -> Secret:
+        """
+        Creates a new Glassflow Secret
+
+        Returns:
+            self: Secret object
+
+        Raises:
+            ValueError: If secret key is not provided in the constructor
+            Unauthorized: If personal access token is invalid
+        """
+        if self.key is None:
+            raise ValueError("Secret key is required in the constructor")
+
+        secret_api_obj = api.CreateSecret(
+            **{
+                "key": self.key,
+                "value": self.value,
+            }
+        )
+
+        endpoint = "/secrets"
+        self._request(
+            method="POST", endpoint=endpoint, json=secret_api_obj.model_dump()
+        )
+        return self
+
+    def delete(self):
+        """
+        Deletes a Glassflow Secret.
+
+        Returns:
+
+        Raises:
+            Unauthorized: If personal access token is invalid
+            SecretNotFound: If secret key does not exist
+            ValueError: If secret key is not provided in the constructor
+        """
+        if self.key is None:
+            raise ValueError("Secret key is required in the constructor")
+
+        endpoint = f"/secrets/{self.key}"
+        self._request(method="DELETE", endpoint=endpoint)
+
+    def _request(
+        self,
+        method,
+        endpoint,
+        request_headers=None,
+        json=None,
+        request_query_params=None,
+        files=None,
+        data=None,
+    ):
+        headers = {**self.headers, **(request_headers or {})}
+        query_params = {**self.query_params, **(request_query_params or {})}
+        try:
+            return super()._request(
+                method=method,
+                endpoint=endpoint,
+                request_headers=headers,
+                json=json,
+                request_query_params=query_params,
+                files=files,
+                data=data,
+            )
+        except errors.UnknownError as http_err:
+            if http_err.status_code == 401:
+                raise errors.SecretUnauthorizedError(
+                    self.key, http_err.raw_response
+                ) from http_err
+            if http_err.status_code == 404:
+                raise errors.SecretNotFoundError(self.key, http_err.raw_response) from http_err
+            raise http_err
