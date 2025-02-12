984951041fb48f1cbe0d0cdeaee8c1dca968593c
Pablo Pardo Garcia <pablo@glassflow.dev>
2025-02-12 13:17:39 +0100

    add docstrings for response data models

--- src/glassflow/models/responses/secret.py
+++ src/glassflow/models/responses/secret.py
@@ -2,9 +2,24 @@ from pydantic import BaseModel
 
 
 class Secret(BaseModel):
+    """
+    Secret response object
+
+    Attributes:
+        key (str): Secret key
+    """
+
     key: str
 
 
 class ListSecretsResponse(BaseModel):
+    """
+    Response from the list secrets endpoint.
+
+    Attributes:
+        total_amount (int): Total amount of the secrets.
+        secrets (list[Secret]): List of secrets.
+    """
+
     total_amount: int
     secrets: list[Secret]
--- src/glassflow/models/responses/space.py
+++ src/glassflow/models/responses/space.py
@@ -4,6 +4,16 @@ from pydantic import BaseModel
 
 
 class Space(BaseModel):
+    """
+    Space response object.
+
+    Attributes:
+        name (str): Space name.
+        id (int): Space id.
+        created_at (datetime): Space creation date.
+        permission (str): Space permission.
+    """
+
     name: str
     id: str
     created_at: datetime
@@ -11,5 +21,13 @@ class Space(BaseModel):
 
 
 class ListSpacesResponse(BaseModel):
+    """
+    Response from list spaces endpoint.
+
+    Attributes:
+        total_amount (int): Total amount of spaces.
+        spaces (List[Space]): List of spaces.
+    """
+
     total_amount: int
     spaces: list[Space]
